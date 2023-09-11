ab254e20f68c5e3bf8ea7cb942938027419fd9e2
Filipe Scaglia <filipecscaglia@gmail.com>
2023-09-11 00:31:29 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto treino de elaboração de um sistema completo do zero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Insira frase motivacional aqui</h1>
+  return <h1>Insira frase motivacional aqui</h1>;
 }
 
 export default Home;
